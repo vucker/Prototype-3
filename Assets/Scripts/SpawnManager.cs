e7273cda933f38551f052eebcd93d8f54a602c05
vucker <103271312+vucker@users.noreply.github.com>
2026-03-11 17:54:33 +0300

    Interval Spawn

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject obstacleObject;
+    public float startDelay;
+    public float intervalDelay;
     private Vector3 spawnPosition = new Vector3(20,0,0);
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(obstacleObject, spawnPosition, Quaternion.identity);
+        InvokeRepeating("SpawnObstacle", startDelay, intervalDelay);
     }
-
-    // Update is called once per frame
-    void Update()
+    private void SpawnObstacle()
     {
-        
+        Instantiate(obstacleObject, spawnPosition, Quaternion.identity);
     }
 }
