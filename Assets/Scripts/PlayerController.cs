4068d71a3a1e1cce46774d66d848caf238548a50
vucker <103271312+vucker@users.noreply.github.com>
2026-03-11 19:29:27 +0300

    После смерти игрок не сможет прыгать

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -22,7 +22,7 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded && !isGameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isGrounded = false;
