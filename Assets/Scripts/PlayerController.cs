ee5054fd7bafc75b9c03c115d9102db38ec1c98b
vucker <103271312+vucker@users.noreply.github.com>
2026-03-11 09:25:52 +0300

    Prevent Double Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     public float jumpForce;
     public float gravityModifier;
+    public bool isGrounded = true;
     private Rigidbody playerRb;
     private void Awake()
     {
@@ -18,8 +19,13 @@ public class PlayerController : MonoBehaviour
     }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) 
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+
+        }
 
     }
+    private void OnCollisionEnter(Collision collision) => isGrounded = true;
 }
