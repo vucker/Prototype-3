5e41499a15f311f9396af3c1afc87f0b12cf541c
vucker <103271312+vucker@users.noreply.github.com>
2026-03-11 18:23:42 +0300

    Переменная repeatWidth для смещения фона

--- Assets/Scripts/RepeatBackground.cs
+++ Assets/Scripts/RepeatBackground.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
     private Vector3 startPos;
+    private float repeatWidth;
+
     void Start()
     {
         startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
     void Update()
     {
-        if (transform.position.x <= startPos.x - 50)
+        if (transform.position.x <= startPos.x - repeatWidth)
             transform.position = startPos;
     }
 }
